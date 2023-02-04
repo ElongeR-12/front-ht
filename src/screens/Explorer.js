283e330444d82ea7f0b45d27b8d05cd2f1e6073d
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2023-02-04 11:57:32 +0100

    image background zone

--- src/screens/Explorer.js
+++ src/screens/Explorer.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, View, ScrollView } from "react-native";
+import ImageBackgroundZone from "../components/ImageBackgroundZone";
 const Explorer = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         {/* image background zone */}
-        <View></View>
+        <ImageBackgroundZone />
         {/* Henri trip description */}
         <View></View>
         {/* planifiez profitez partagez */}
