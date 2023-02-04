eb9f8a4c8f619121964d19a89ae2ac9c83781111
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2023-02-04 14:11:21 +0100

    push develop branch

---
+++ src/components/DetailsButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const DetailsButton = ({ setSeeMore, seeMore }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setSeeMore((prev) => !prev)}
+      >
+        <Text style={styles.color}>
+          {seeMore ? "Réduire" : "En savoir plus"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default DetailsButton;
+const styles = StyleSheet.create({
+  container: { alignItems: "center", marginTop: 20 },
+  button: {
+    backgroundColor: "#062e70",
+    marginBottom: 40,
+    width: Dimensions.get("screen").width / 2,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    borderRadius: 50,
+  },
+
+  color: { color: "#ffffff" },
+});
